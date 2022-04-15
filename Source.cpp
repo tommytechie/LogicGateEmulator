ab2ae0415f8f74ec84f5bb3ecfaa9bdcbb25340e
TommyLau00 <tommy@fotogallery.hk>
2022-04-15 03:07:26 +0100

    It works now, calling and retrieving results from hierarchy
    But still cannot directly put the truth table there to test it because I haven't implemented a way to assign inputs directly from the top object...
    
    Wondering if I should write another Class just for handling it over the Driver Code side, over the main function, instead of fiddling with the existing code in the LogicGate Class.
    
    Really satisfied with the result, and really thank the guy from discord telling me why I couldn't do it last time, because I didn't reference the object so the objects were copied, hence no results could be modified.
    
    This can be built into a really handy framework to handle similar stuff that requires calling deep into the hierarchy using objects, cuz it's more flexible and you can retrieve a large varieties of information from an object, so it's not just like a recursive function that works like a tree.
    
    I will try to implement memoization and other dynamic programming tricks into similar frameworks to see if it helps with the performance a lot.
    
    For now, I need to make the truth table works on this before moving onto other stuff.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,4 @@
 #include "tommylogic.h"
-#include "objectgate.h"
 
 using std::vector; using std::cout; using std::endl; using std::string;
 using tommylogic::generateTruthtableXY;
@@ -14,148 +13,60 @@ AND----OR----NAND----NOR----XNOR----XOR
 
 */
 
-void gateLoop(LogicGate inputGate, int NumOfInputs, int i);
-
 int main() {
-	
-	RecurObjTest obj11("obj11"), obj12("obj12"), obj13("obj13"), obj14("obj14");
-	RecurObjTest obj21("obj21"), obj22("obj22");
-	RecurObjTest obj31("obj31");
+
+	LogicGate obj11("obj11"), obj12("obj12"), obj13("obj13"), obj14("obj14"), obj15("obj15"), obj16("obj16"), obj17("obj17"), obj18("obj18");
+	LogicGate obj21("obj21"), obj22("obj22"), obj23("obj23"), obj24("obj24");
+	LogicGate obj31("obj31"), obj32("obj32");
+	LogicGate obj41("obj41");
+
+	obj11.assignFunctions(0);
+	obj12.assignFunctions(1);
+	obj13.assignFunctions(2);
+	obj14.assignFunctions(3);
+	obj15.assignFunctions(0);
+	obj16.assignFunctions(1);
+	obj17.assignFunctions(2);
+	obj18.assignFunctions(3);
+
+	obj11.input(0);
+	obj12.input(1);
+	obj13.input(1, 0);
+	obj14.input(0, 0);
+	obj15.input(1);
+	obj16.input(0);
+	obj17.input(1, 1);
+	obj18.input(0, 1);
 
 	obj21.input({ &obj11,&obj12 });
 	obj22.input({ &obj13,&obj14 });
+	obj23.input({ &obj15,&obj16 });
+	obj24.input({ &obj17,&obj18 });
 
 	obj31.input({ &obj21,&obj22 });
+	obj32.input({ &obj23,&obj24 });
 
-	obj31.calling();
+	obj41.input({ &obj31,&obj32 });
+
+	obj41.call();
+
+	obj41.print();
 
 	obj31.print();
+	obj32.print();
+
 	obj21.print();
 	obj22.print();
+	obj23.print();
+	obj24.print();
+
 	obj11.print();
 	obj12.print();
 	obj13.print();
 	obj14.print();
+	obj15.print();
+	obj16.print();
+	obj17.print();
+	obj18.print();
 
 }
-
-void gateLoop(LogicGate inputGate, int NumOfInputs, int iterations) {
-	//0 Yes, 1 And, 2 Or, 3 Not, 4 Nand, 5 Nor, 6 Xnor, 7 Xor
-	vector<vector<int>> TheTruthTable = generateTruthtableXY(NumOfInputs);
-	for (int i = 0; i < iterations; i++) {
-		srand(time(0) * i);
-		int Rand = rand() % (int)pow(2, NumOfInputs);
-		cout << "Gate Name: " << inputGate.name << " | " << "Rand: " << Rand << endl;
-		vector<int> inputarr = TheTruthTable[Rand];
-		cout << "input arr: "; printVector<int>(inputarr); cout << endl;
-		inputGate.inputValues(inputarr); //TODO Figure out assigning values to internal objects
-		if (inputGate.ObjectTypeIdentifier == 1)inputGate.parallelCalc();
-		else if (inputGate.ObjectTypeIdentifier == 2)inputGate.Calc(); //TODO calculate them and Figure out passing out internal objects' outputs correctly
-		else cout << "Complex Gates coming soon..." << endl;
-		inputGate.printValue();
-		cout << endl;
-	}
-}
-
-//TODO find out how many inputs the obj inputs and how many outputs the obj outputs and then process it inside the big object and then output accordingly...
-//TODO Map the Outputs of the Object Gates to the Input of the Bigger Gate itself (current Object)
-//TODO Figure out how many Inputs should itself take, for it to be used for other bigger gates in the future.
-//TODO Map the future Inputs into the smaller inside object gates...
-
-/*  
-	//class logic1 - 6...
-	//Example Driver Code:
-
-	logic6 L6(""); logic4 L4(""); logic3 L3(""); logic2 L2(""); logic1 L1("");
-
-	L2.testInput(input2, logic2("AND")); cout << endl;
-	L2.testInput(input2, logic2("OR")); cout << endl;
-	L1.testInput(input1, logic1("NOT")); cout << endl;
-	L2.testInput(input2, logic2("NAND")); cout << endl;
-	L2.testInput(input2, logic2("NOR")); cout << endl;
-	L2.testInput(input2, logic2("XNOR")); cout << endl;
-	L2.testInput(input2, logic2("XOR")); cout << endl;
-	L4.run(input4, logic4("TEST")); cout << endl;
-
-	printTruthtable(4, input4, L4.result);
-
-	cout << "L6: " << endl;
-	L6.run(input6, logic6("TEST2"));
-	printTruthtable(6, input6, L6.result);
-	cout << "=================" << endl;
-
-	///L3.testInput(input3, logic3("TEST1"));
-	//L3.run(input3, logic3("TEST1"));
-	//printTruthtable(3, input3, L3.result);
-
-*/
-
-/*
-	//class ObjGate...
-	//Example Driver Code:
-
-	ObjGate AND(1, 0); ObjGate OR(1, 1); ObjGate AND1; ObjGate AND2;
-	AND.define(1); OR.define(2); AND1.define(1); AND2.define(1);
-
-	cout << "AND: " << AND.returnValue() << endl;
-	cout << "OR: " << OR.returnValue() << endl;
-
-	cout << "==============" << endl;
-	AND2.a(AND1.a(&AND, &OR),1);
-
-	cout << "AND1 " << AND1.returnValue() << endl;
-
-	cout << "AND2 " << AND2.returnValue() << endl;
-*/
-
-/*
-	//class LogicGate...
-	//Example Driver Code! Unfinished.
-
-	cout << "====INPUT====" << endl;
-	for (int i = 0; i < obj1.input.size(); i++) {
-		cout << i << ". " << obj1.input[i] << endl;
-	}
-
-	cout << "====POS====" << endl;
-	for (int i = 0; i < obj1.position.size(); i++) {
-		cout << obj1.position[i] << endl;
-	}
-
-	//for (int i = 0; i < obj2.position.size(); i++) {
-	//	cout << obj2.position[i] << endl;
-	//}
-
-	cout << "====OUTPUT====" << endl;
-	obj1.parallelCalc();
-
-	for (int i = 0; i < obj1.output.size(); i++) {
-		cout << obj1.output[i] << endl;
-	}
-*/
-
-/*
-
-LogicGate obj1; LogicGate obj2; LogicGate obj3; LogicGate multi1; LogicGate multi2; LogicGate Complex1;
-
-	//obj1.inputValues({ 1,1,1,0 });
-
-	obj1.define({ 1,2,3 }); obj1.setName("AND,OR,NOT");
-	obj2.define({ 2,1,3 }); obj2.setName("OR,AND,NOT");
-	obj3.define({ 1,1 }); obj3.setName("AND,AND");
-
-	multi1.define({ obj1, obj2, obj3 }); multi1.setName("multi-1");
-	multi2.define({ obj3, obj2, obj1 }); multi2.setName("multi-2");
-
-	Complex1.define({ multi1,multi2 }); Complex1.setName("Complex-1");
-
-	gateLoop(obj1, 5, 10);
-	gateLoop(multi1, 14, 5);
-	gateLoop(multi2, 14, 5);
-	//gateLoop(Complex1, 28, 10);
-
-
-	cout << "NumOfInputs: " << multi1.NumOfInputs << endl;
-	cout << "NumOfOutputs: " << multi1.NumOfOutputs << endl;
-
-*/
--- tommylogic.h
+++ tommylogic.h
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <stdexcept>
 
-using std::vector; using std::cout; using std::endl; using std::string;
+using std::vector; using std::cout; using std::endl; using std::string; using std::pair;
 namespace tommylogic {
 	bool vectorSizeChecker(int InputSize, vector<vector<int>> InputVector) { //Check V[i][?], the size of the row to see if it matches InputSize
 		for (int i = 0; i < InputVector.size(); i++) {
@@ -86,387 +86,115 @@ namespace tommylogic {
 
 using tommylogic::printVector; using tommylogic::vectorSizeChecker; using tommylogic::printTruthtable;
 
-class logic1 {
-	vector<int> testresult;
-public:
-	vector<int> result;
+class LogicGate {
+private:
 
-	string GN;
-	vector<string> GateNameArr = { "NOT" };
-	logic1(string GateName) {
-		GN = GateName;
-		vector<string>::iterator it = std::find(GateNameArr.begin(), GateNameArr.end(), GN);
-		if (it != GateNameArr.end()) {
-			//gate found
-		}
-		else {
-			//gate not found
-			GN = "error";
-		}
+	vector<LogicGate*> ObjList;
+	vector<string> ObjNames;
+
+	pair<int, int> Input;
+	vector<int> Output;
+
+	int (LogicGate::* TempFunc) (pair<int, int> inputvalue);
+
+	int YES(pair<int, int> inputvalue) {
+		return inputvalue.first;
 	}
-	int operator()(int A) {
-		if (GN == "NOT")return NOT(A);
-		else { return 0; }
+	int AND(pair<int, int> inputvalue) {
+		return (inputvalue.first == 1 && inputvalue.second == 1);
 	}
-	template <class _Fn>
-	void testInput(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(1, inputArr) == false) { cout << "Input Array has wrong size(s) in logic1 run" << endl; return; };
-		if (_Func.GN != "error") { cout << "Truth Table for " << _Func.GN << " gate:" << endl; }
-		else { cout << "WRONG GATE NAME" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			cout << inputArr[i][0] << " => " << _Func(inputArr[i][0]) << endl;
-		}
+	int OR(pair<int, int> inputvalue) {
+		return (inputvalue.first == 1 || inputvalue.second == 1);
 	}
-	template <class _Fn>
-	void run(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(1, inputArr) == false) { cout << "Input Array has wrong size(s) in logic1 run" << endl; return; };
-		if (_Func.GN != "error") { /*run without error*/ }
-		else { cout << "_WRONG GATE NAME_" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			result.push_back(_Func(inputArr[i][0]));
-		}
-	}
-
-
-	int NOT(int A) {
-		if (A == 1)return 0;
+	int NOT(pair<int, int> inputvalue) {
+		if (inputvalue.first != 0) return 0;
 		else return 1;
 	}
-};
 
-class logic2 : public logic1 {
-	vector<int> testresult;
 public:
-	vector<int> result;
+	string Name = "";
 
-	string GN;
-	vector<string> GateNameArr = { "AND", "OR", "NAND", "NOR", "XNOR", "XOR" };
-	logic2(string GateName) : logic1(GateName) {
-		GN = GateName;
-		vector<string>::iterator it = std::find(GateNameArr.begin(), GateNameArr.end(), GN);
-		if (it != GateNameArr.end()) {
-			//gate found
-		}
-		else {
-			//gate not found
-			GN = "error";
-		}
-	}
-	int operator()(int A, int B) {
-		if (GN == "AND")return AND(A, B);
-		else if (GN == "OR")return OR(A, B);
-		else if (GN == "NOT")return NOT(A);
-		else if (GN == "NAND")return NAND(A, B);
-		else if (GN == "NOR")return NOR(A, B);
-		else if (GN == "XNOR")return XNOR(A, B);
-		else if (GN == "XOR")return XOR(A, B);
-		else { return 0; }
-	}
-	template <class _Fn>
-	void testInput(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(2, inputArr) == false) { cout << "Input Array has wrong size(s) in logic2 testInput" << endl; return; };
-		if (_Func.GN != "error") { cout << "Truth Table for " << _Func.GN << " gate:" << endl; }
-		else { cout << "WRONG GATE NAME" << endl; return; }
+	int GotCalled = 0;
+	int GotTriggered = 0;
+	int ObjListSize = 0;
 
-		for (int i = 0; i < inputArr.size(); i++) {
-			cout << inputArr[i][0] << " " << inputArr[i][1] << " => " << _Func(inputArr[i][0], inputArr[i][1]) << endl;
-		}
+	LogicGate(string name) {
+		Name = name;
 	}
-	template <class _Fn>
-	void run(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(2, inputArr) == false) { cout << "Input Array has wrong size(s) in logic2 run" << endl; return; };
-		if (_Func.GN != "error") { /*run without error*/ }
-		else { cout << "_WRONG GATE NAME_" << endl; return; }
 
-		for (int i = 0; i < inputArr.size(); i++) {
-			result.push_back(_Func(inputArr[i][0], inputArr[i][1]));
+	void call() {
+		if (ObjList.size() == 0) {
+			Trigger();
+			return;
+		}
+		Called();
+	}
+
+	void assignFunctions(int inputgates) {
+		switch (inputgates)
+		{
+		case 0:
+			TempFunc = &LogicGate::YES;
+			break;
+		case 1:
+			TempFunc = &LogicGate::NOT;
+			break;
+		case 2:
+			TempFunc = &LogicGate::AND;
+			break;
+		case 3:
+			TempFunc = &LogicGate::OR;
+			break;
+		default:
+			cout << "WARNING: Wrong Function Assigned" << endl;
+			TempFunc = &LogicGate::YES;
+			break;//Nothing...
 		}
 	}
 
-	int AND(int A, int B) {
-		if (A == 1 && B == 1)return 1;
-		else return 0;
+	void Trigger() { //to calculate using fundamental gates
+		GotTriggered++;
+
+		this->Output.push_back((this->*TempFunc)(this->Input));
 	}
-	int OR(int A, int B) {
-		if (A == 1 || B == 1)return 1;
-		else return 0;
+
+	void Called() { //to summarize calculations from lower objects
+		GotCalled++;
+		for (int i = 0; i < ObjList.size(); i++) {
+			ObjList[i]->call(); //Recursive calling into the hierarchy
+
+			this->ObjNames.push_back(ObjList[i]->Name);//Get ObjNames down from the hierarchy, put into this-> ObjNames array.
+
+			for (int j = 0; j < this->ObjList[i]->Output.size(); j++) {//Get Fundamental Gates result from their Output down from the hierarchy, put into this-> Output array
+				this->Output.push_back(ObjList[i]->Output[j]);
+			}
+		}
 	}
-	int NAND(int A, int B) {
-		return NOT(AND(A, B));
+
+	void input(int first) { //for NOT and YES gates
+		Input.first = first;
+		Input.second = 2;
 	}
-	int NOR(int A, int B) {
-		return NOT(OR(A, B));
+	void input(int first, int second) { //other gates
+		Input.first = first;
+		Input.second = second;
 	}
-	int XNOR(int A, int B) {
-		if (AND(A, B) == 1 || NOR(A, B) == 1)return 1;
-		else return 0;
+	void input(vector<LogicGate*> objList) { //Complex Object Gate
+		ObjList = objList;
 	}
-	int XOR(int A, int B) {
-		if (OR(A, B) == 1 && NAND(A, B) == 1)return 1;
-		else return 0;
+
+	void print() {
+		cout << Name << " Got Called: " << GotCalled << endl;
+		cout << Name << " Got Triggered: " << GotTriggered << endl;
+		cout << "Output: ";
+		for (int i = 0; i < Output.size(); i++) {
+			cout << Output[i] << " ";
+		}
+		cout << endl;
+		cout << "Object Names: ";
+		for (int i = 0; i < this->ObjList.size(); i++) {
+			cout << ObjNames[i] << " ";
+		}
+		cout << endl << "==============" << endl;
 	}
 };
-
-class logic3 : public logic2 {
-	vector<int> testresult;
-public:
-	vector<int> result;
-
-	string GN;
-	vector<string> GateNameArr = { "TEST1" };
-	logic3(string GateName) : logic2(GateName) {
-		GN = GateName;
-		vector<string>::iterator it = std::find(GateNameArr.begin(), GateNameArr.end(), GN);
-		if (it != GateNameArr.end()) {
-			//gate found
-		}
-		else {
-			//gate not found
-			GN = "error";
-		}
-	}
-	int operator()(int A, int B, int C) {
-		if (GN == "TEST1")return TEST1(A, B, C);
-		else { return 0; }
-	}
-
-	//Template Functions with a Functor as Parameter
-	template <class _Fn>
-	void testInput(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(3, inputArr) == false) { cout << "Input Array has wrong size(s) in logic3 testInput" << endl; return; };
-		testresult.clear(); //testInput will destroy the data in testresult
-		if (_Func.GN != "error") { cout << "Truth Table for " << _Func.GN << " gate:" << endl; }
-		else { cout << "WRONG GATE NAME" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			testresult.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2]));
-			cout << inputArr[i][0] << " " << inputArr[i][1] << " " << inputArr[i][2] << " => " << testresult[i] << endl;
-		}
-	}
-	template <class _Fn>
-	void run(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(3, inputArr) == false) { cout << "Input Array has wrong size(s) in logic3 run" << endl; return; };
-		if (_Func.GN != "error") { /*run without error*/ }
-		else { cout << "_WRONG GATE NAME_" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			result.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2]));
-		}
-	}
-
-	int TEST1(int A, int B, int C) {
-		return OR(AND(OR(NOT(A),B),NOR(B,AND(A,NOT(C)))),AND(A,NAND(B, C)));
-	}
-};
-
-class logic4 : public logic3 {
-	vector<int> testresult;
-public:
-	vector<int> result;
-
-	string GN;
-	vector<string> GateNameArr = { "TEST" };
-	logic4(string GateName) : logic3(GateName) {
-		GN = GateName;
-		vector<string>::iterator it = std::find(GateNameArr.begin(), GateNameArr.end(), GN);
-		if (it != GateNameArr.end()) {
-			//gate found
-		}
-		else {
-			//gate not found
-			GN = "error";
-		}
-	}
-	int operator()(int A, int B, int C, int D) {
-		if (GN == "TEST")return TEST(A, B, C, D);
-		else { return 0; }
-	}
-
-	//Template Functions with a Functor as Parameter
-	template <class _Fn>
-	void testInput(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(4, inputArr) == false) { cout << "Input Array has wrong size(s) in logic4 testInput" << endl; return; };
-		testresult.clear(); //testInput will destroy the data in testresult
-		if (_Func.GN != "error") { cout << "Truth Table for " << _Func.GN << " gate:" << endl; }
-		else { cout << "WRONG GATE NAME" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			testresult.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2], inputArr[i][3]));
-			cout << inputArr[i][0] << " " << inputArr[i][1] << " " << inputArr[i][2] << " " << inputArr[i][3] << " => " << testresult[i] << endl;
-		}
-	}
-	template <class _Fn>
-	void run(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(4, inputArr) == false) { cout << "Input Array has wrong size(s) in logic4 run" << endl; return; };
-		if (_Func.GN != "error") { /*run without error*/ }
-		else { cout << "_WRONG GATE NAME_" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			result.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2], inputArr[i][3]));
-		}
-	}
-
-	int TEST(int A, int B, int C, int D) {
-		return NOT(XOR(AND(A, B), OR(C, D)));
-	}
-};
-
-class logic6 : public logic4 {
-	vector<int> testresult;
-public:
-	vector<int> result;
-
-	string GN;
-	vector<string> GateNameArr = { "TEST1","TEST2" };
-	logic6(string GateName) : logic4(GateName) {
-		GN = GateName;
-		vector<string>::iterator it = std::find(GateNameArr.begin(), GateNameArr.end(), GN);
-		if (it != GateNameArr.end()) {
-			//gate found
-		}
-		else {
-			//gate not found
-			GN = "error";
-		}
-	}
-	int operator()(int A, int B, int C, int D, int E, int F) {
-		if (GN == "TEST1")return TEST1(A, B, C, D);
-		if (GN == "TEST2")return TEST2(A, B, C, D, E, F);
-		else { return 0; }
-	}
-
-	//Template Functions with a Functor as Parameter
-	template <class _Fn>
-	void testInput(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(6, inputArr) == false) { cout << "Input Array has wrong size(s) in logic6 testInput" << endl; return; };
-		testresult.clear(); //testInput will destroy the data in testresult
-		if (_Func.GN != "error") { cout << "Truth Table for " << _Func.GN << " gate:" << endl; }
-		else { cout << "WRONG GATE NAME" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			testresult.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2], inputArr[i][3], inputArr[i][4], inputArr[i][5]));
-			cout << inputArr[i][0] << " " << inputArr[i][1] << " " << inputArr[i][2] << " " << inputArr[i][3] << " " << inputArr[i][4] << " " << inputArr[i][5] << " => " << testresult[i] << endl;
-		}
-	}
-	template <class _Fn>
-	void run(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(6, inputArr) == false) { cout << "Input Array has wrong size(s) in logic6 run" << endl; return; };
-		if (_Func.GN != "error") { /*run without error*/ }
-		else { cout << "_WRONG GATE NAME_" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			result.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2], inputArr[i][3], inputArr[i][4], inputArr[i][5]));
-		}
-	}
-
-	int TEST1(int A, int B, int C, int D) {
-		return NOT(XOR(AND(A, B), OR(C, D)));
-	}
-
-	int TEST2(int A, int B, int C, int D, int E, int F) {
-		return NOT(XOR(AND(A, B), OR(C, D)));
-	}
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* JUST A REMINDER ON WHAT TO CHANGE IN CASE MY STUPID BRAIN FORGETS IT... ££<<===££
-
-class logic6 ££<<===££ : public logic4 ££<<===££ {
-	vector<int> testresult;
-public:
-	vector<int> result;
-
-	string GN;
-	vector<string> GateNameArr = { "TEST1","TEST2" };
-	logic6££<<===££(string GateName) : logic4££<<===££(GateName) {
-		GN = GateName;
-		vector<string>::iterator it = std::find(GateNameArr.begin(), GateNameArr.end(), GN);
-		if (it != GateNameArr.end()) {
-			//gate found
-		}
-		else {
-			//gate not found
-			GN = "error";
-		}
-	}
-	int operator()(int A, int B, int C, int D, int E, int F) ££<<===££ {
-		if (GN == "TEST1")return TEST1(A, B, C, D); //OF COURSE CHANGE, THESE ARE GATES
-		if (GN == "TEST2")return TEST2(A, B, C, D, E, F);
-		else { return 0; }
-	}
-
-	//Template Functions with a Functor as Parameter
-	template <class _Fn>
-	void testInput(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(6 ££<<===££ , inputArr) == false) { cout << "Input Array has wrong size(s) in logic6 ££<<===££ testInput" << endl; return; };
-		testresult.clear(); //testInput will destroy the data in testresult
-		if (_Func.GN != "error") { cout << "Truth Table for " << _Func.GN << " gate:" << endl; }
-		else { cout << "WRONG GATE NAME" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			testresult.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2], inputArr[i][3], inputArr[i][4], inputArr[i][5])); ££<<===££
-			cout << inputArr[i][0] << " " << inputArr[i][1] << " " << inputArr[i][2] << " " << inputArr[i][3] << " " << inputArr[i][4] << " " << inputArr[i][5] << " => " << testresult[i] << endl; ££<<===££
-		}
-	}
-	template <class _Fn>
-	void run(vector<vector<int>> inputArr, _Fn _Func) {
-		if (vectorSizeChecker(6 ££<<===££ , inputArr) == false) { cout << "Input Array has wrong size(s) in logic6 ££<<===££ run" << endl; return; };
-		if (_Func.GN != "error") {  }
-		else { cout << "_WRONG GATE NAME_" << endl; return; }
-
-		for (int i = 0; i < inputArr.size(); i++) {
-			result.push_back(_Func(inputArr[i][0], inputArr[i][1], inputArr[i][2], inputArr[i][3], inputArr[i][4], inputArr[i][5])); ££<<===££
-		}
-	}
-
-	int TEST1(int A, int B, int C, int D) { //THESE ARE GATES! OF COURSE CHANGE!
-		return NOT(XOR(AND(A, B), OR(C, D)));
-	}
-
-	int TEST2(int A, int B, int C, int D, int E, int F) {
-		return NOT(XOR(AND(A, B), OR(C, D)));
-	}
-
-
-};
-
-*/
